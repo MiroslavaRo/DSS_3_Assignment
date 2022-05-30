13c991f3e968e193d588311c4ef8e3ad59963486
Miroslava <miroslavaroshchina@gmail.com>
2022-05-30 20:16:36 +0300

    port

--- src/index.js
+++ src/index.js
@@ -12,17 +12,7 @@ require('./database/mongoose');
 app.use(express.json());
 app.use('/contacts', contactRouter, error)
 app.use('/users', userRouter, error)
-app.get('/help', (req, res) => {
 
-    res.status(200).send({
-        title: "Help Page",
-        details: "Lorem Ipsum...",
-        index: 1,
-        date: new Date()
-
-    });
-
-});
 
 app.get('*', (req, res) => {
 
@@ -33,10 +23,7 @@ app.get('*', (req, res) => {
     });
 
 });
-var port = process.env.PORT || 3002;
-app.listen(port, function () {
-    console.log("Server is listening on port " + port);
+app.listen(PORT_NUMBER, () => {
+    console.log(`Server is listening on port ${PORT_NUMBER}`);
 });
-/*app.listen(PORT_NUMBER, () => {
-    console.log("Server is listening on port " + PORT_NUMBER);
-});*/
+
